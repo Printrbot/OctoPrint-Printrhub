5f960634f96002c921d038070bcbaf1b0a586391
Brian Fife <bdfife@gmail.com>
2018-04-29 20:18:36 +0000

    Ok, got the image links working (not sure what clicked) and moved the stylesheet to LESS. The stylesheet isn't automatically handled by the helper code and isn't busted down to CSS automagically.

--- octoprint_Printrhub/__init__.py
+++ octoprint_Printrhub/__init__.py
@@ -7,13 +7,22 @@ import octoprint.plugin
 
 class PrintrhubUI(octoprint.plugin.StartupPlugin,
                   octoprint.plugin.UiPlugin,
-                  octoprint.plugin.TemplatePlugin):
+                  octoprint.plugin.TemplatePlugin,
+                  octoprint.plugin.AssetPlugin):
 
     def on_after_startup(self):
         self._logger.info("Printrhub UI successfully running.")
         
     def will_handle_ui(self, request):
         return True
+
+    def get_assets(self):
+        # fixme: before making "prod" commit, change config yaml
+        # to bundle, and check in CSS files. 
+        return dict(
+            css=["css/Printrhub.css"],
+            less=["less/Printrhub.less"]
+        )
     
     def on_ui_render(self, now, request, render_kwargs):
         from flask import make_response, render_template
