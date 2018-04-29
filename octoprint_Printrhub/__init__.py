880a81bc37392bc936f3600bae2ad461ae6dcc7a
Brian Fife <bdfife@gmail.com>
2018-04-29 18:01:27 +0000

    Checking in current progress. I've made two runs at this, one (successfully) overriding the format so that everything is moved to the tab-bar... this is not reflected in the code but described here: https://gist.github.com/foosel/86157172899666cc0658.
    Second, I've made some progress overriding the default UI. I assume the best practice for actually interacting with the OCTOPRINT system is to use the API. I've gotten hung on on how to load static assets like images, there doesn't seem to be a good example of how to do this and all conversation centers around js/css static files.

--- octoprint_Printrhub/__init__.py
+++ octoprint_Printrhub/__init__.py
@@ -3,35 +3,22 @@ from __future__ import absolute_import
 
 import octoprint.plugin
 
-# Ok, let's make sense out of the naming convention:
-# Plugin folder is "filetab"
-# Plugn name is not set explicitly.
-# Plugin implementation/function is FiletabPlugin()
-# Tab callout in config.yaml is plugin_filetab
-# Jinja template name is "filetab_tab.jinja2"
+# Plugin identifier is "Printrhub"
 
-# in my case, plugin folder is Octorprint-Printrhub
-# Plugin name is "Printrhub"
-
-
-class PrintrhubUI(octoprint.plugin.StartupPlugin, octoprint.plugin.UiPlugin, octoprint.plugin.TemplatePlugin):
+class PrintrhubUI(octoprint.plugin.StartupPlugin,
+                  octoprint.plugin.UiPlugin,
+                  octoprint.plugin.TemplatePlugin):
 
     def on_after_startup(self):
         self._logger.info("Printrhub UI successfully running.")
-
+        
     def will_handle_ui(self, request):
-        return False
-
-    def get_template_configs(self):
-        return [
-            dict(type="tab", name="Files", template="printrhub_tab.jinja2")
-        ]
+        return True
     
     def on_ui_render(self, now, request, render_kwargs):
         from flask import make_response, render_template
-        #self._logger.info("Keywords  %s", % str(render_kwargs.keys())
         return make_response(render_template("printrhub_web.jinja2",
-                                             **render_kwargs))
-                          
+                                             render_kwargs=render_kwargs))
+
 __plugin_name__ = "Printrhub"
 __plugin_implementation__ = PrintrhubUI()
