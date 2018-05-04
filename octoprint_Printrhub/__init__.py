325e97474f73869ab839f99b7a58299999f10868
Brian Fife <bdfife@gmail.com>
2018-05-04 10:53:59 +0000

    Working towards getting file upload working. Here's the URL plumbing.

--- octoprint_Printrhub/__init__.py
+++ octoprint_Printrhub/__init__.py
@@ -2,13 +2,36 @@
 from __future__ import absolute_import
 
 import octoprint.plugin
+import flask # maybe clean this up.
 
 # Plugin identifier is "Printrhub"
 
 class PrintrhubUI(octoprint.plugin.StartupPlugin,
                   octoprint.plugin.UiPlugin,
                   octoprint.plugin.TemplatePlugin,
-                  octoprint.plugin.AssetPlugin):
+                  octoprint.plugin.AssetPlugin,
+                  octoprint.plugin.BlueprintPlugin):
+
+    @octoprint.plugin.BlueprintPlugin.route("/upload", methods=["POST"])
+    def upload_file(self):
+        self._logger.info("File upload page")
+
+        # fixme: understand why this call requires an API key
+        # (and then ignores the actual value)
+        input_name = "file"
+        keys = ("name", "size", "content_type", "path")
+
+        result = dict(
+            found_file=False,
+        )
+
+        for key in keys:
+            param = input_name + "." + key
+            if param in flask.request.values:
+                result["found_file"] = True
+                result[key] = flask.request.values[param]
+
+        return flask.jsonify(result)
 
     def on_after_startup(self):
         self._logger.info("Printrhub UI successfully running.")
@@ -16,6 +39,7 @@ class PrintrhubUI(octoprint.plugin.StartupPlugin,
     def will_handle_ui(self, request):
         return True
 
+        
     def get_assets(self):
         # fixme: before making "prod" commit, change config yaml
         # to bundle, and check in CSS files. 
@@ -25,15 +49,34 @@ class PrintrhubUI(octoprint.plugin.StartupPlugin,
         )
     
     def on_ui_render(self, now, request, render_kwargs):
+        self._logger.info("Calling UI_Render")
+        self._logger.info(request.path)
+        self._logger.info(request.method)
+
         from flask import make_response, render_template
 
-        self._logger.info("Instance name is: " + self._identifier)
-
+        # Generate data for file view.
+        # fixme: only needed when viewing files.
         file_data = self._file_manager.list_files()
-        self._logger.info(file_data)
+        #self._logger.info(file_data)
+
         return make_response(render_template("printrhub_web.jinja2",
                                              render_kwargs=render_kwargs,
                                              files=file_data))
 
+    def bodysize_hook(self, current_max_body_sizes, *args, **kwargs):
+        # fixme: do the math and pick a reasonable size.
+        return [("POST", r"/upload", 1024 * 1024)]
+    
+
 __plugin_name__ = "Printrhub"
-__plugin_implementation__ = PrintrhubUI()
+# __plugin_implementation__ = PrintrhubUI()
+
+def __plugin_load__():
+    global __plugin_implementation__
+    global __plugin_hooks__
+
+    __plugin_implementation__ = PrintrhubUI()
+    __plugin_hooks__ = {
+        "octoprint.server.http.bodysize": __plugin_implementation__.bodysize_hook
+    }
