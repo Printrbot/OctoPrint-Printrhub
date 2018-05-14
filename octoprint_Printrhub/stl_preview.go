8e533e67a3653b6fea60e2f248388d3a688dbff2
Brian Fife <bdfife@gmail.com>
2018-05-14 01:53:42 +0000

    Updated fauxGL preview options to match those used on printrbot.cloud.

--- octoprint_Printrhub/stl_preview.go
+++ octoprint_Printrhub/stl_preview.go
@@ -1,4 +1,9 @@
 package main
+
+// This file was updated with settings that match the values
+// oddirmeyer used in Printrbot Cloud... but there's no automated
+// process to keep these in sync right now. 
+
 import (
        .
        "github.com/fogleman/fauxgl"
@@ -7,18 +12,20 @@ import (
 )
 
 const (
-      scale	= 1
-      width 	= 540
-      height 	= 480
-      fovy	= 30	// vertical field of view degrees
-      near	= 1  	// near clipping plane
-      far	= 10 	// far cliping plane
+      scale		  = 4
+      near		  = 1  	// near clipping plane
+      far	    	  = 10 	// far cliping plane
+      defaultFovy   	  = 40.0
+      defaultWidth 	  = 540
+      defaultHeight 	  = 480
+      defaultWorldColor	  = "E0D9CC"
+      defaultObjectColor  = "F09500" 
 )
 
 var   (
-      eye	= V(-3, 1, 0.75)		// camera position
-      center	= V(0, -0.07, 0)	   	// view center position
-      up	= V(0, 1, 0)  		   	// up vector
+      eye	= V(3, 1, 1.75)			// camera position
+      center	= V(0, 0, 0)			// view center position
+      up	= V(0, 0, 1)  		   	// up vector
       light 	= V(-0.75, 1, 0.25).Normalize()	// light direction
 )
 
@@ -26,7 +33,8 @@ func main () {
 
      	inputFile  := flag.String("input", "", "STL file to render")
      	outputFile := flag.String("output", "out.png", "output PNG file")
-     	colorVal   := flag.String("color", "#468966", "Base color for object")
+     	colorVal   := flag.String("color", defaultObjectColor,
+		      	          "Base color for object")
 
      	flag.Parse()
 
@@ -38,21 +46,27 @@ func main () {
 
 	// fit mesh in a bi-unit cube centered at the origin
 	mesh.BiUnitCube()
-
+	// rotate on z-axis
+	mesh.Transform(Rotate(V(0, 0, 1.0), Radians(-90)))
 	// smooth the normals
-	mesh.SmoothNormalsThreshold(Radians(30))
+	//mesh.SmoothNormalsThreshold(Radians(30))
 
 	// create a rendering context
-	context := NewContext(width*scale, height*scale)
-	context.ClearColorBufferWith(HexColor("#FFF8E3"))
+	context := NewContext(defaultWidth*scale, defaultHeight*scale)
+	context.ClearColorBufferWith(HexColor(defaultWorldColor))
 
 	// create transformation matrix and light direction
-	aspect := float64(width) / float64(height)
-	matrix := LookAt(eye, center, up).Perspective(fovy, aspect, near, far)
+	aspect := float64(defaultWidth) / float64(defaultHeight)
+	matrix := LookAt(eye, center, up).Perspective(defaultFovy, aspect,
+						      near, far)
+        light := V(3.5, 1, 1.5).Normalize()
 
 	// use builtin phong shader
 	shader := NewPhongShader(matrix, light, eye)
 	shader.ObjectColor = HexColor(*colorVal)
+	shader.DiffuseColor = Gray(0.8)
+	shader.SpecularColor = Gray(0.55)
+	shader.SpecularPower = 90
 	context.Shader = shader
 
 	// render
@@ -60,7 +74,8 @@ func main () {
 
 	// downsample image for antialiasing
 	image := context.Image()
-	image = resize.Resize(width, height, image, resize.Bilinear)
+	image = resize.Resize(defaultWidth, defaultHeight, image,
+	                      resize.Bilinear)
 
 	// save image
 	SavePNG(*outputFile, image)
